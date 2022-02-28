c3b474089a9ace030d22f1484fa7d7a94e657841
RidOne673 <76752207+RidOne673@users.noreply.github.com>
2022-03-01 01:38:16 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,263 @@
+const help = (prefix, botname, pushname2, uangku, role, sender, ucapan1, times) => {
+return `🔥𝙾 𝚌 𝚝 𝚟 - 𝚝 𝚎 𝚌 𝚑  
+𝙾 𝚏 𝚏 𝚒 𝚌 𝚒 𝚊 𝚕🔥
+Just gabuts!
+
+${ucapan1}
+
+📢  *INFO*  📢
+📍 *Bot Name* : *${botname}*
+📍 *Prefix* : 「  *${prefix}*  」
+📍 *Nama* : *${pushname2}*
+📍 *Duit* : *Rp.${uangku}*.-
+📍 *Role* : *${role}*
+📍 *Nomer* : *wa.me/${sender.split("@")[0]}*
+📍 ${times}
+
+Hay *${pushname2}* 👋🏻, jangan lupa jaga kesehatan ya...
+tetap patuhi prokes dan ikuti vaksinasi
+Mari mendukung program pemerintah dalam menanggulangi COVID-19 di Indonesia🇲🇨
+
+Baca rules bot dahulu sebelum menggunakan fitur-fitur bot agar tidak terbanned dengan cara ketik *${prefix}rules*
+
+Ada keperluan dengan owner bot?
+ketik *${prefix}chatowner* pesan kamu
+
+Ketik *.menu* untuk melihat menu permainan
+
+*M e n u* :
+
+Fun:
+📌 *${prefix}truth*
+📌 *${prefix}dare*
+📌 *${prefix}dadu*
+📌 *${prefix}bacot*
+📌 *${prefix}addbacot*
+📌 *${prefix}halah*
+📌 *${prefix}hilih*
+📌 *${prefix}huluh*
+📌 *${prefix}heleh*
+📌 *${prefix}holoh*
+📌 *${prefix}mutual*
+📌 *${prefix}asupan*
+📌 *${prefix}fancy*   
+📌 *${prefix}ayla*
+📌 *${prefix}cerpen*
+📌 *${prefix}simi*
+📌 *${prefix}bucin*
+📌 *${prefix}ppcouple*
+
+Sticker:
+📌 *${prefix}ttp*
+📌 *${prefix}attp*
+📌 *${prefix}sticker*
+
+Quotes:
+📌 *${prefix}quoteskehidupan*
+📌 *${prefix}quotesislami*
+📌 *${prefix}quotesnasehat*
+📌 *${prefix}quotesmotivasi*
+📌 *${prefix}quotesimg*
+
+Info:
+📌 *${prefix}infogempa*
+📌 *${prefix}cuaca*
+📌 *${prefix}jadwaltv*
+📌 *${prefix}spekhp*
+📌 *${prefix}ml*
+📌 *${prefix}covid*
+
+Download:
+📌 *${prefix}play*
+📌 *${prefix}playmp4*
+📌 *${prefix}ytmp3*
+📌 *${prefix}ytmp4*
+📌 *${prefix}tik*
+📌 *${prefix}ig*
+📌 *${prefix}igstory*
+📌 *${prefix}twit*
+📌 *${prefix}fb*   [err]
+
+Primbon:
+📌 *${prefix}artinama*
+📌 *${prefix}artimimpi*
+📌 *${prefix}nomorhoki*
+📌 *${prefix}jodoh*
+📌 *${prefix}zodiakharian*
+📌 *${prefix}zodiak*
+
+Your Info:
+📌 *${prefix}buylimit*
+📌 *${prefix}buypremiumlimit*
+📌 *${prefix}dompet*
+📌 *${prefix}myinfo*
+📌 *${prefix}limit*
+📌 *${prefix}changelog*
+
+Song:
+📌 *${prefix}lirik*
+📌 *${prefix}chord*
+
+Search:
+📌 *${prefix}wikipedia*
+📌 *${prefix}brainly*
+📌 *${prefix}playstore*
+📌 *${prefix}image*
+📌 *${prefix}pinterest*
+
+Question:
+📌 *${prefix}bisakah*
+📌 *${prefix}kapankah*
+📌 *${prefix}gantengcek*
+📌 *${prefix}cantikcek*
+📌 *${prefix}rate*
+📌 *${prefix}apakah*
+📌 *${prefix}watak*
+📌 *${prefix}hobby*
+
+Religion:
+📌 *${prefix}randomquran*
+📌 *${prefix}listsurah*
+📌 *${prefix}quran*
+📌 *${prefix}qaudio*
+📌 *${prefix}jadwalsholat*
+📌 *${prefix}tafsirquran*
+📌 *${prefix}kisahnabi*
+
+Group Admin:
+📌 *${prefix}group lock*
+📌 *${prefix}group unlock*
+📌 *${prefix}simih*
+📌 *${prefix}welcome*
+📌 *${prefix}nsfw*
+📌 *${prefix}leveling*
+📌 *${prefix}badword*
+📌 *${prefix}antilink*
+📌 *${prefix}add* [628xxxxx]
+📌 *${prefix}kick* [tag]
+📌 *${prefix}kicktime* [tag]
+📌 *${prefix}promote* [tag]
+📌 *${prefix}demote* [tag]
+📌 *${prefix}hidetag*
+📌 *${prefix}tagall*
+📌 *${prefix}tagall2*
+📌 *${prefix}tagall3*
+📌 *${prefix}setpp*
+📌 *${prefix}linkgroup*
+📌 *${prefix}resetlink*
+📌 *${prefix}pengumuman*
+📌 *${prefix}on*
+📌 *${prefix}off*
+📌 *${prefix}bukatime*
+📌 *${prefix}tutuptime*
+📌 *${prefix}tutup*
+
+Group:
+📌 *${prefix}listadmin*
+📌 *${prefix}tagme*
+📌 *${prefix}hidetag1*
+📌 *${prefix}groupinfo*
+📌 *${prefix}blocklist*
+📌 *${prefix}listbadword*
+📌 *${prefix}delete*
+📌 *${prefix}banlist*
+📌 *${prefix}bundir*
+📌 *${prefix}ganteng*
+📌 *${prefix}cantik*
+📌 *${prefix}beban*
+📌 *${prefix}babi*
+📌 *${prefix}jadian*
+📌 *${prefix}level*
+📌 *${prefix}afk*
+
+Owner:
+📌 *${prefix}bc*
+📌 *${prefix}clone* [tag]
+📌 *${prefix}setprefix*
+📌 *${prefix}clearall*
+📌 *${prefix}leave*
+📌 *${prefix}simih*
+📌 *${prefix}ohidetag*
+📌 *${prefix}ban* [tag]
+📌 *${prefix}addban*
+📌 *${prefix}unban* [tag]
+📌 *${prefix}kickban* [tag]
+📌 *${prefix}ochat*
+📌 *${prefix}addbadword*
+📌 *${prefix}delbadword*
+📌 *${prefix}transfer*
+📌 *${prefix}giftlimit*
+📌 *${prefix}join*
+
+Maker:
+📌 *${prefix}avengers*
+📌 *${prefix}thunder*
+📌 *${prefix}bpink*
+📌 *${prefix}glitch*
+📌 *${prefix}glitch2*
+📌 *${prefix}tahta*
+📌 *${prefix}marvel*
+📌 *${prefix}matrix*
+📌 *${prefix}phlogo*
+📌 *${prefix}greenhoror*
+📌 *${prefix}dropwater*
+📌 *${prefix}devil*
+📌 *${prefix}badut*
+📌 *${prefix}kalender*
+
+Tools:
+📌 *${prefix}ocr*
+📌 *${prefix}tomp3*
+📌 *${prefix}nulis*
+📌 *${prefix}nulis2*
+📌 *${prefix}img2url*
+📌 *${prefix}wame*
+📌 *${prefix}toimg*
+📌 *${prefix}privatechat*
+📌 *${prefix}nobg*
+📌 *${prefix}ssweb*
+📌 *${prefix}shortlink*
+📌 *${prefix}translate*
+📌 *${prefix}morse*
+📌 *${prefix}igstalk*
+
+Anime:
+📌 *${prefix}waifu*
+📌 *${prefix}husbu*
+📌 *${prefix}wait*
+📌 *${prefix}anime*
+📌 *${prefix}loli*
+📌 *${prefix}kusonime*
+📌 *${prefix}anime*
+
+Images:
+📌 *${prefix}pictcewek*
+📌 *${prefix}pictcowok*
+📌 *${prefix}pictkucing*
+📌 *${prefix}kpop*
+
+Other:
+📌 *${prefix}tts*
+📌 *${prefix}codebahasa*
+📌 *${prefix}jumlah*
+📌 *${prefix}terbalik*
+📌 *${prefix}cersex*
+📌 *${prefix}fancy*
+📌 *${prefix}darkjoke*
+
+Thanks to:
+•Allah swt
+•Mhankbarbar
+•Penyedia Layanan API
+•Orang-orang yang Berdonasi
+•Hans
+•Wifi Tetangga
+
+Jika menemukan bug atau request fitur dan join bot harap chat owner dengan cara:
+*${prefix}chatowner* [pesan].
+
+Nb:bot ini masih terus dikembangkan.
+`
+}
+
+exports.help = help
---
+++ src/menu.js
@@ -0,0 +1,95 @@
+const menu = (prefix, pushname2, uangku, role, sender, limitCounts) => {
+	return `🔥𝙾 𝚌 𝚝 𝚟 - 𝚝 𝚎 𝚌 𝚑  
+	𝙾 𝚏 𝚏 𝚒 𝚌 𝚒 𝚊 𝚕🔥
+  
+  ❗MODERATION MODE❗
+  
+  *MENU*:
+  
+  ╠➥ *${prefix}kick*
+  ~>Ketik ${prefix}kick lalu tag member yang ingin dikeluarkan
+  
+  ╠➥ *${prefix}kicktime*
+  ~>Ketik ${prefix}kicktime lalu tag member yang ingin dikeluarkan dan memberi jeda waktu sebelum mengeluarkan
+  
+  ╠➥ *${prefix}add*
+  ~>Ketik ${prefix}add lalu masukkan nomor member yang akan dimasukkan, format nomor harus 628xxxxx, contoh 
+  ${prefix}add 6285773929493
+  
+  ╠➥ *${prefix}group*
+  ~>Ketik ${prefix}group lock untuk menutup group dan ${prefix}group unlock untuk membuka group
+  
+  ╠➥ *${prefix}tutuptime*
+  ~>Menutup group dengan waktu, cara penggunaan contoh: ${prefix}tutuptime 10 detik
+  
+  ╠➥ *${prefix}bukatime*
+  ~>Membuka group dengan waktu, cara penggunaan contoh: ${prefix}bukatime 10 detik
+  
+  ╠➥ *${prefix}tutup*
+  ~>Menutup group dan otomatis membuka group setelah waktu yg di stel habis, cara penggunaan: ${prefix}tutup 10 detik
+  
+  ╠➥ *${prefix}linkgc*
+  ~>Ketik ${prefix}linkgc untuk memunculkan link group
+  
+  ╠➥ *${prefix}hidetag*
+  ~>Ketik ${prefix}hidetag text, untuk memberi notif ke semua member group
+  
+  ╠➥ *${prefix}setpp*
+  ~>Kirim gambar lalu ketik ${prefix}setpp untuk mengubah icon group
+  
+  ╠➥ *${prefix}welcome*
+  ~>Ketik ${prefix}welcome 1 untuk mengaktifkan mode welcome dan ketik ${prefix}welcome 0 untuk menonaktifkan mode welcome
+  
+  ╠➥ *${prefix}tagall*
+  ~>Ketik ${prefix}tagall untuk memanggil semua member group, terdapat 3 fitur ${prefix}tagall, ${prefix}tagall2, ${prefix}tagall3
+  
+  ╠➥ *${prefix}pengumuman*
+  ~>Ketik ${prefix}pengumuman text pengumuman untuk memberi notifikasi pengumuman kepada member group
+  
+  ╠➥ *${prefix}leave*
+  ~>Ketik ${prefix}leave untuk mengeluarkan bot dari group
+  
+  ╠➥ *${prefix}promote*
+  ~>Ketik ${prefix}promote lalu tag member yang ingin dijadikan admin
+  
+  ╠➥ *${prefix}demote*
+  ~>Ketik ${prefix}demote lalu tag admin yang ingin di jadikan member
+  
+  ╠➥ *${prefix}antilink*
+  ~>Ketik ${prefix}antilink 1 untuk mengaktifkan dan ketik ${prefix}antilink untuk menonaktifkan
+  
+  ╠➥ *${prefix}antidelete*
+  ~>Ketik ${prefix}antidelete 1 untuk mengaktifkan dan ketik ${prefix}antidelete untuk menonaktifkan
+  
+  ╠➥ *${prefix}badword*
+  ~>Ketik ${prefix}badword 1 untuk mengaktifkan dan ketik ${prefix}badword untuk menonaktifkan
+  
+  ╠➥ *${prefix}leveling*
+  ~>Ketik ${prefix}leveling 1 untuk mengaktifkan dan ketik ${prefix}leveling untuk menonaktifkan
+  
+  ╠➥ *${prefix}nsfw*
+  ~>Ketik ${prefix}nsfw 1 untuk mengaktifkan dan ketik ${prefix}nsfw untuk menonaktifkan
+  
+  ╠➥ *${prefix}on/off*
+  ~>Untuk mengaktifkan dan menonaktifkan bot di group dengan cara ketik ${prefix}on atau ${prefix}off
+  
+  *BONUS MENU:*
+  
+  ╠➥ *${prefix}buylimit*
+  ~>untuk membeli limit jika limit telah habis, dengan harga 2000/limit. Cara pembelian ketik ${prefix}buylimit jumlah limit yang akan dibeli
+  
+  ╠➥ *${prefix}brainly*
+  ~>Ketik${prefix}brainly lalu tulis pertanyaan kamu
+  
+  ╠➥ *${prefix}img2url*
+  ~>Kirim atau reply gambar dan beri caption ${prefix}img2url untuk mengubah gambar menjadi link
+  
+  ╠➥ *${prefix}randomquran*
+  ~>Ketik ${prefix}randomquran untuk memunculkan quran random
+  
+  ╠➥ *${prefix}cmd*
+  ~>Ketik ${prefix}cmd untuk melihat all fitur
+	`
+}
+
+exports.menu = menu
